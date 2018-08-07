1faef993588c1f21d13d6dede4f1d04153aed9ef
Abdullah Al Hossain <hello2441139abcd@gmail.com>
2018-08-06 22:59:42 -0700

    Second time

---
+++ 10783/OddSum.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    while(sc.hasNext())
+    {
+      int total=sc.nextInt();
+      for(int j=1; j<=total; j++)
+      {
+        int sum=0;
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        while(a<=b)
+        {
+          if(a%2!=0)
+          {
+            sum+=a;
+          }
+          a++;
+        }
+        System.out.printf("Case %d: %d\n",j,sum);
+      }
+    }
+  }
+}
